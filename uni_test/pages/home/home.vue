43badb1175210a5b7d0e58895b40cd75bd29343a
fang <2548275503@qq.com>
2019-03-14 11:14:15 +0800

    123

--- uni_test/pages/home/home.vue
+++ uni_test/pages/home/home.vue
@@ -11,14 +11,26 @@
 			<swiper-item>
 				<view class="swiper-item uni-bg-blue">C</view>
 			</swiper-item>
+			 <image style="width: 200px; height: 200px; background-color: #eeeeee;" src="../../static/cart.png"></image>
 		</swiper>
+		<uni-grid :data="[
+			{image:'https://img-cdn-qiniu.dcloud.net.cn/img/shu.png',text:'圣诞树'},
+			{image:'https://img-cdn-qiniu.dcloud.net.cn/img/lindang.png',text:'铃铛'},
+			{image:'https://img-cdn-qiniu.dcloud.net.cn/img/laoren.png',text:'圣诞老人'},
+			{image:'https://img-cdn-qiniu.dcloud.net.cn/img/liwu.png',text:'礼物'},
+			{image:'https://img-cdn-qiniu.dcloud.net.cn/img/maozi.png',text:'帽子'},
+			{image:'https://img-cdn-qiniu.dcloud.net.cn/img/shoutao.png',text:'手套'}]" 
+			show-border="false">
+		</uni-grid>
 	</view>
 </template>
 
 <script>
 	import uniNavBar from "@/components/uni-nav-bar.vue"
+	import uniGrid from "@/components/uni-grid/uni-grid.vue"
+
 	export default {
-		components: {uniNavBar},
+		components: {uniNavBar,uniGrid},
 		data() {
 			return {
 				background: ['color1', 'color2', 'color3'],
